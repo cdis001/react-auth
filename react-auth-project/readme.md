f83667f13f7253ee3b115a4e389ef1cdb6411328
cdis001 <shopingid@naver.com>
2023-01-09 22:40:56 +0900

    update. login page react-hook-form - 로그인 페이지 input react-hook-form으로 변경

--- react-auth-project/readme.md
+++ react-auth-project/readme.md
@@ -41,9 +41,34 @@
 > https://reactrouter.com/en/main/routers/create-browser-router
 
 - localStorage로 관리하던 로그인 여부를 recoil로 관리하도록 업데이트
+
   - 개인적으로는 초기값 설정이 가능한 부분이 편리하다고 생각함.
     - localStorage에서는 가장 처음 시작하는 페이지에 setItem()을 사용해야 했는데, 이게 살짝 꼬이거나 하면 답이 없었기 때문에... 명시적으로 초기값을 설정하는 부분이 편리함
   - 타입을 설정할 수 있는 부분도 편리
     - 이 전에 localStorage에서는 boolean 타입을 잘못 줬다가 페이지 이동이 미친듯이 되는 오류가 있었어서..(main <-> login 무한반복) string 타입으로 설정해줬었는데 recoil에서는 명확히 타입을 지정할 수 있어, 비교문 사용시 좀 더 편리
   - 이 프로젝트에서는 느끼지 못 하는 부분이긴 하지만ㅋㅋ 이런 여러 상태들을 한 곳에 모아놓고 볼 수 있는것도 편리!
     - redux를 사용했을 때 처럼 여러 상태들을 한 곳에서 관리 할 수 있어 보기도 편하고 코드 짜기도 편리할듯
+
+- 로그인 페이지 React Hook Form 사용
+
+  - 기존 AuthInputBox로 사용하던 input을 React Hook Form으로 대체
+  - 일단... 기존 input도 component로 분리하면서 로직이 좀 꼬였는데 AuthInputBox를 React Hook Form으로 대체할 생각을 하니 눈 앞이 캄캄해서 일단 로그인 페이지만 바꿔봄
+  - 장점으로는 state를 줄일 수 있다는 점. 반복되는 함수들(setState와 같은..)을 처리하지 않아서 코드가 좀 깔끔해진 느낌
+  - 컴포넌트화 시키기 편했으면 좋겠다. 보통 이런 폼의 경우에는 한 곳에서만 쓰이는게 아니라서(로그인/회원가입/유저정보...) 스타일을 통일시킬 필요가 있는데 컴포넌트화 시키기 어려우면 사용되는 페이지들에 스타일을 따로 두던가.. css를 따로 만들던가 해야할 듯
+    - 지금 제일 걱정은 submit 함수를 어떻게 컴포넌트화 시켜야 하는지가 고민...
+  - submit 하니 생각났는데, handleSubmit 함수는 리턴값이 함수인...함수..(뭐라 하는지 잘 모르겠음;;) 함수를 리턴하는 함수로 이루어짐.
+
+    - 리턴값이 함수이면서, 파라미터를 함수로 받는 고차함수로 이루어짐.
+    - 프로퍼티는 각각 onSubmit과 onError를 처리할 수 있는 함수들로 이루어짐
+    - 예제를 보니, onSubmit에서 에러를 처리해도 되고(비동기식), 함수를 따로 나눠도 괜찮게끔(동기식) 설계한 듯
+    - 아래와 같이 파라미터로 원격으로 함수를 전달한 뒤 바로 실행해도 괜찮고, 바로 함수를 넣어도 괜찮음
+
+    ```javascript
+    // It can be invoked remotely as well
+    handleSubmit(onSubmit)();
+
+    // You can pass an async function for asynchronous validation.
+    handleSubmit(async (data) => await fetchAPI(data));
+    ```
+
+    - 유효성 검사와 에러를 다루기 쉽다고 하는데, 그 부분은 회원가입에 적용한 뒤에 봐야 알 듯
--- react-auth-project/src/pages/Login/index.tsx
+++ react-auth-project/src/pages/Login/index.tsx
@@ -2,24 +2,57 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
+import { useForm } from "react-hook-form";
 
 import { isLoginState } from "../../recoil/atoms";
 import AuthBox from "../../components/AuthBox";
-import AuthInputBox from "../../components/AuthInputBox";
+// import AuthInputBox from "../../components/AuthInputBox";
 import AuthButton from "../../components/AuthButton";
 import AuthButtonList from "../../components/AuthButtonList";
 
 import { login } from "../../__users/api";
 
+const AuthInput = styled.input`
+  height: 38px;
+  padding: 8px 12px;
+  box-sizing: border-box;
+  margin: 10px 0;
+  border: 1px solid #dbdbdb;
+  border-radius: 4px;
+
+  &::placeholder {
+    color: #ccc;
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid #949494;
+  }
+
+  & > p {
+    margin: 0;
+    color: #dc2626;
+    font-size: 12px;
+  }
+
+  & > .hidden {
+    visibility: hidden;
+  }
+`;
+
+type LoginDatas = {
+  userId: string;
+  password: string;
+};
+
 const Login = () => {
-  const [userId, setUserId] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const { register, handleSubmit } = useForm<LoginDatas>();
   const [isLogin, setIsLogin] = useRecoilState(isLoginState);
 
   const navigate = useNavigate();
 
-  const userLoginBtn = () => {
-    const { status, message = "" } = login({ userId, password });
+  const userLoginBtn = handleSubmit((d) => {
+    const { status, message = "" } = login(d);
     if (status === "success") {
       setIsLogin(true);
       alert("Login Success!");
@@ -27,7 +60,7 @@ const Login = () => {
     } else {
       alert(message);
     }
-  };
+  });
 
   useEffect(() => {
     if (isLogin) {
@@ -39,18 +72,11 @@ const Login = () => {
     <section>
       <h1>LOGIN PAGE</h1>
       <AuthBox>
-        <AuthInputBox
-          value={userId}
-          setValue={setUserId}
-          placeholder={"Email or username"}
-          // errorMessage={"This email or username is not available."}
-        />
-        <AuthInputBox
-          value={password}
-          setValue={setPassword}
+        <AuthInput placeholder={"Email or username"} {...register("userId")} />
+        <AuthInput
           placeholder={"password"}
-          type={"password"}
-          // errorMessage={"This password is not available."}
+          type="password"
+          {...register("password")}
         />
         <AuthButton onClick={userLoginBtn} title={`로그인`} />
         <AuthButtonList>
