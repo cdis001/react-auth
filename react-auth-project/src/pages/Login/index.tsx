c5a0f7e93a9773d0f4211f76f91e2021f130be50
cdis001 <shopingid@naver.com>
2023-01-17 19:57:42 +0900

    style. remove AuthInput styled-components - 로그인 페이지에 있던 사용하지 않는 AuthInput 스타일 컴포넌트 삭제

--- react-auth-project/src/pages/Login/index.tsx
+++ react-auth-project/src/pages/Login/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { useForm } from "react-hook-form";
@@ -11,34 +10,6 @@ import AuthButton from "../../components/AuthButton";
 import AuthButtonList from "../../components/AuthButtonList";
 
 import { login } from "../../__users/api";
-
-const AuthInput = styled.input`
-  height: 38px;
-  padding: 8px 12px;
-  box-sizing: border-box;
-  margin: 10px 0;
-  border: 1px solid #dbdbdb;
-  border-radius: 4px;
-
-  &::placeholder {
-    color: #ccc;
-  }
-
-  &:focus {
-    outline: none;
-    border: 1px solid #949494;
-  }
-
-  & > p {
-    margin: 0;
-    color: #dc2626;
-    font-size: 12px;
-  }
-
-  & > .hidden {
-    visibility: hidden;
-  }
-`;
 
 type LoginDatas = {
   userId: string;
